63f6a7899e13c8d6c9637a89da7ee44492eb08fa
Sadek Baroudi <sadekbaroudi@gmail.com>
2024-09-03 23:10:28 -0700

    ximega was in the vial repo, but no vial keymap or configuration

---
+++ keyboards/fingerpunch/ximega/v1/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0xC7, 0x18, 0x97, 0x0E, 0x35, 0xD9, 0xA2, 0x98}
+
+#ifndef VIAL_INSECURE
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+#endif
+
+// For via/vial only
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
